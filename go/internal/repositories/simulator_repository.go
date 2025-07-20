1f9e839acc785b81c449866ad11275f3580a02b0
omemoji <me@omemoji.com>
2025-07-21 01:31:47 +0900

    [update]レビュー修正

--- go/internal/repositories/simulator_repository.go
+++ go/internal/repositories/simulator_repository.go
@@ -18,7 +18,7 @@ type FilePaths struct {
 
 type SimulatorRepository struct {
 	FilePaths FilePaths
-	responce  models.Simulator
+	response  models.Simulator
 }
 
 func NewSimulatorRepository(filePaths FilePaths) *SimulatorRepository {
@@ -50,43 +50,51 @@ func (r *SimulatorRepository) Search() (models.Simulator, error) {
 		r.FilePaths.FilePathWaist,
 		r.FilePaths.FilePathLeg,
 	} {
-		file, err := os.Open(filePath)
+		equipmentList, err := processFile(filePath)
 		if err != nil {
 			return models.Simulator{}, err
 		}
-		defer file.Close()
-
-		reader := csv.NewReader(file)
-		records, err := reader.ReadAll()
-		if err != nil {
-			return models.Simulator{}, err
-		}
-
-		for _, record := range records {
-			if record[0][0] == '#' {
-				continue
-			}
-			equipment := models.Equipment{
-				Name:             record[0],
-				Gender:           MustAtoi(record[1]),
-				Type:             MustAtoi(record[2]),
-				Rarity:           MustAtoi(record[3]),
-				Slots:            MustAtoi(record[4]),
-				PortAvailable:    DecideAvailable(record[5]),
-				VillageAvailable: DecideAvailable(record[6]),
-				InitialDefense:   MustAtoi(record[7]),
-				FinalDefense:     MustAtoi(record[8]),
-				Resistance:       [5]int{MustAtoi(record[9]), MustAtoi(record[10]), MustAtoi(record[11]), MustAtoi(record[12]), MustAtoi(record[13])},
-				Skills: [5]models.Skill{
-					{Name: record[14], Points: MustAtoi(record[15])},
-					{Name: record[16], Points: MustAtoi(record[17])},
-					{Name: record[18], Points: MustAtoi(record[19])},
-					{Name: record[20], Points: MustAtoi(record[21])},
-					{Name: record[22], Points: MustAtoi(record[23])},
-				},
-			}
-			r.responce.EquipmentLists[i] = append(r.responce.EquipmentLists[i], equipment)
-		}
+		r.response.EquipmentLists[i] = equipmentList
 	}
-	return r.responce, nil
+	return r.response, nil
+}
+
+func processFile(filePath string) ([]models.Equipment, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	var equipmentList []models.Equipment
+	for _, record := range records {
+		if record[0][0] == '#' {
+			continue
+		}
+		equipment := models.Equipment{
+			Name:             record[0],
+			Gender:           MustAtoi(record[1]),
+			Type:             MustAtoi(record[2]),
+			Rarity:           MustAtoi(record[3]),
+			Slots:            MustAtoi(record[4]),
+			PortAvailable:    DecideAvailable(record[5]),
+			VillageAvailable: DecideAvailable(record[6]),
+			InitialDefense:   MustAtoi(record[7]),
+			FinalDefense:     MustAtoi(record[8]),
+			Resistance:       [5]int{MustAtoi(record[9]), MustAtoi(record[10]), MustAtoi(record[11]), MustAtoi(record[12]), MustAtoi(record[13])},
+			Skills: [5]models.Skill{
+				{Name: record[14], Points: MustAtoi(record[15])},
+				{Name: record[16], Points: MustAtoi(record[17])},
+				{Name: record[18], Points: MustAtoi(record[19])},
+				{Name: record[20], Points: MustAtoi(record[21])},
+				{Name: record[22], Points: MustAtoi(record[23])},
+			},
+		}
+		equipmentList = append(equipmentList, equipment)
+	}
+	return equipmentList, nil
 }
